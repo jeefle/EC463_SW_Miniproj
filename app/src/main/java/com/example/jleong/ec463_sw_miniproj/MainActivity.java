f488cb461b117f74103d89c251edbbd9420cf896
Tariq Ahsan <tahsan@bu.edu>
2018-09-18 19:53:21 -0400

    not working query to firebase for root node

---
+++ app/src/main/java/com/example/jleong/ec463_sw_miniproj/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.jleong.ec463_sw_miniproj;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference dbrooms;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        TextView tv = findViewById(R.id.textView);
+
+        dbrooms = FirebaseDatabase.getInstance().getReference("rooms");
+
+        tv.setText(dbrooms.toString());
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/jleong/ec463_sw_miniproj/RoomInfo.java
@@ -0,0 +1,28 @@
+package com.example.jleong.ec463_sw_miniproj;
+
+/**
+ * Created by tahsan on 9/18/18.
+ */
+
+public class RoomInfo {
+    String number;
+    String time;
+    Integer temp;
+    Integer humidity;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public Integer getTemp() {
+        return temp;
+    }
+
+    public Integer getHumidity() {
+        return humidity;
+    }
+}
