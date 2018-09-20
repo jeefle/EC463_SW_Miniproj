52ac18876054c8537c9ebd3a108e689d927085be
jeefle <31052127+jeefle@users.noreply.github.com>
2018-09-19 21:48:28 -0400

    Update RoomInfo.java

--- app/src/main/java/com/example/jleong/ec463_sw_miniproj/RoomInfo.java
+++ app/src/main/java/com/example/jleong/ec463_sw_miniproj/RoomInfo.java
@@ -1,28 +1,55 @@
-package com.example.jleong.ec463_sw_miniproj;
+package com.example.jeff.ec463swmp1;
 
-/**
- * Created by tahsan on 9/18/18.
- */
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
-public class RoomInfo {
-    String number;
-    String time;
-    Integer temp;
-    Integer humidity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-    public String getNumber() {
-        return number;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    TextView textDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+        textDisplay = findViewById(R.id.textDisplay);
+        ReadRoom();
     }
 
-    public String getTime() {
-        return time;
-    }
+    private void ReadRoom() {
+        DocumentReference user = db.collection("rooms").document("room0")
+                .collection("climate").document("hum0");
+        user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot doc = task.getResult();
+                    StringBuilder fields = new StringBuilder("");
+                    fields.append("room0, hum0, 00:00").append(doc.get("00:0 "));
+                    textDisplay.setText(fields.toString());
+                    Log.d("SAHDAOSIDHAISOd", fields.toString());
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
 
-    public Integer getTemp() {
-        return temp;
-    }
-
-    public Integer getHumidity() {
-        return humidity;
+                    }
+                });
     }
 }
+
